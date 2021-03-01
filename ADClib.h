c5fd97966bac89600cff82edf37abd53f6ec002b
AudioDIWHY <40243825+cslammy@users.noreply.github.com>
2021-03-01 07:33:16 -0800

    Update ADClib.h
    changed variable defs to use stdint.h

--- ADClib.h
+++ ADClib.h
@@ -1,8 +1,8 @@
 #ifndef ADCLIB_H_
-
+#include <stdint.h>
 //LIBRARY to do 10 or 8 bit reads from Atmel MPU ADC
 
-//initialize ADC (set sample rate etc). 
+//initialize ADC (set sample rate etc).
 //no return.
 
 void ADC_init();
@@ -10,13 +10,13 @@ void ADC_init();
 //return 8 bit value.  This is
 //contents of 8 LSBs.
 
-unsigned char analogRead8bit();
+uint8_t analogRead8bit();
 
 //return an int of 10 LSBs from ADC.
 //don't forget to set one of the analog portCs as input.
 //otherwise this won't work.
 //hard coded to use MUX channel 0.
-unsigned int analogRead10bit();
+uint16_t analogRead10bit();
 
 
 #endif
